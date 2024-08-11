463cb403c5b42abc4a923fd35a51da60744f9985
arata-Engineer <arataspace1@gmail.com>
2024-08-11 11:57:44 +0900

    commit_function02

--- function.php
+++ function.php
@@ -1,3 +1,7 @@
+<?php
+// 型宣言と異なるデータ型の場合にエラーを発生させる
+declare(strict_types=1)
+?>
 <!DOCTYPE html>
  <html lang="ja">
  
@@ -28,7 +32,65 @@
         // 夜の挨拶を出力する関数を呼び出す
         say_good_evening();
         ?>
-     </p>
+        </p>
+        <p>
+            <?php
+            // 与えられた引数$price二送料を加算し、その値を出力する関数を定義する
+            function calculate_total($price) {
+                $total = $price + 500;
+
+                echo $total . '円<br>';
+            }
+
+            // 関数を呼び出し、引数として購入金額を渡す
+            calculate_total(1200);
+
+            // 与えられた引数$priceと引数$shipping_feeを加算し、その値を出力する関数を定義する
+            function add_two_arguments($price, $shipping_fee) {
+                $total = $price + $shipping_fee;
+
+                echo $total . '円<br>';
+
+            }
+
+            // 関数を呼び出し、引数として購入金額と送料を渡す
+            add_two_arguments(1200, 500);
+            ?>
+        </p>
+        <p>
+            <?php
+            // 与えられた引数$numを2倍にし、その値を戻り値として返す引数を定義する
+            function double($num) {
+                return $num * 2;
+            }
+
+            // 関数の戻り値を出力する
+            echo double(30);
+            ?>
+        </p>
+        <p>
+            <?php
+            // 引数の型宣言を行い、引数を整数系に限定する
+            function type_hinting_argument(int $num) {
+                return $num * 2;
+
+            }
+
+            // 型宣言と同じデータ型の引数を渡す
+            echo type_hinting_argument(1);
+            ?>
+        </p>
+        <p>
+            <?php
+            // 戻り値の型宣言を行い、戻り値を誠意数型に限定する
+            function type_hinting_return_value($num): int {
+                return $num * 2;
+            }
+
+            // 型宣言と同じデータ型の戻り値が返ってくるようにする
+            echo type_hinting_return_value(1);
+            ?>
+        </p>
  </body>
  
  </html>
